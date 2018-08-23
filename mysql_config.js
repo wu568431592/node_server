266778351094f8e39d0886233766f7e8176c5425
wu568431592 <wuwenchao0314@163.com>
2018-08-23 16:34:40 +0800

    commit

--- mysql_config.js
+++ mysql_config.js
@@ -2,10 +2,11 @@ var mysql  = require('mysql');
 
 var pool = mysql.createPool({
     connectionLimit : 50,
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'forever',
+    host     : 'tvhcm60x.2302.dnstoo.com',
+    user     : 'forever1314_f',
+    password : 'wuwenchao',
+    database : 'forever1314',
+    port: 5505,
     multipleStatements : true  //是否允许执行多条sql语句
 });
 
